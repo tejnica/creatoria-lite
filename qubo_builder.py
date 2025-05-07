61ccee459c3e2ed66252f69a82786c1044288281
tejnica <tejnica@gmail.com>
2025-05-08 00:56:47 +0300

    qubo_builder.py

--- qubo_builder.py
+++ qubo_builder.py
@@ -1,11 +1,17 @@
 
 import yaml
 from pyqubo import Binary
+
 def build_qubo(path='contradiction.yaml'):
     spec = yaml.safe_load(open(path))
-    x = Binary('x')
-    w = spec['options']['cell_thickness']['weight']
-    e = abs(spec['options']['cell_thickness']['effect_lambda'])
-    H = w*x + e*x
-    Q,_ = H.compile().to_qubo()
-    return Q
+    H = 0      # гамильтониан
+    binaries = {}
+    # проходим по опциям и создаём биты
+    for name, cfg in spec["options"].items():
+        # если bits == 1 — один бит; можно потом обобщить на N
+        x = Binary(name)
+        binaries[name] = x
+        H += cfg["weight"] * x + abs(cfg["effect_lambda"]) * x
+    Q, _ = H.compile().to_qubo()
+    return Q, binaries, spec
+

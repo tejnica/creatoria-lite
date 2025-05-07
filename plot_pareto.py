c2ccde151b1e3eebf1939476b069817b1160326e
tejnica <tejnica@gmail.com>
2025-05-08 01:49:46 +0300

    Add files via upload

--- plot_pareto.py
+++ plot_pareto.py
@@ -1,11 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
-import pandas as pd, matplotlib.pyplot as plt
-from pymoo.util.nds.non_dominated_sorting import NonDominatedSorting
-def plot(csv='results.csv'):
-    df = pd.read_csv(csv)
-    nd = NonDominatedSorting().do(df[['lambda_neg','mass']].values, only_non_dominated_front=True)
-    plt.scatter(df['lambda_neg'], df['mass'], alpha=.3)
-    plt.scatter(df.loc[nd,'lambda_neg'], df.loc[nd,'mass'], marker='*', s=80)
-    plt.xlabel('-lambda'); plt.ylabel('mass'); plt.tight_layout(); plt.savefig('pareto.png')
-if __name__=='__main__':
+def plot():
+    df = pd.read_csv("results.csv")
+    df = df.drop_duplicates()
+
+    # Pareto front: lower lambda_neg and lower mass are better
+    pareto = df.sort_values(['lambda_neg', 'mass'], ascending=[True, True])
+    pareto_front = [pareto.iloc[0]]
+    for i in range(1, len(pareto)):
+        if pareto.iloc[i]['mass'] < pareto_front[-1]['mass']:
+            pareto_front.append(pareto.iloc[i])
+    pareto_front = pd.DataFrame(pareto_front)
+
+    plt.figure(figsize=(6, 5))
+    plt.scatter(df["lambda_neg"], df["mass"], color="orange", alpha=0.8, label="Candidates", marker="*")
+    plt.plot(pareto_front["lambda_neg"], pareto_front["mass"], color="black", label="Pareto front")
+    plt.xlabel("-lambda")
+    plt.ylabel("mass")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("pareto.png")
+
+if __name__ == "__main__":
     plot()
